56967e4c5a77b54a67be73f428f0b31544c7f8f7
qnchoyy <qni191488@abv.bg>
2024-10-06 17:00:48 +0300

    add hardcoded movie (test)

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,16 @@
-const movies = []
+const movies = [
+    {
+        title: 'The Little Marmaid',
+        genre: 'Fantasy',
+        director: 'Rob Marshall',
+        date: '2023',
+        imageUrl: 'img/the-little-mermaid.jpg',
+        rating: '5',
+        description: 'amazing',
+    }
+]
 
 exports.create = (movieData) => {
+    console.log(movieData);
     movies.push(movieData);
 }
