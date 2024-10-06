6e5cdac8bc7f33ed6c52557c0d567ff4063d5834
qnchoyy <qni191488@abv.bg>
2024-10-06 14:08:41 +0300

    add create movie page

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const homeConroller = require('./controllers/homeController');
+const movieController = require('./controllers/movieController')
 
 router.use(homeConroller);
+router.use(movieController);
 
 module.exports = router;
