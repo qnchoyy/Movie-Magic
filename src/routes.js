ad3a16a057eab13bff4461be4a2ec1d15d8c75d8
qnchoyy <qni191488@abv.bg>
2024-10-06 14:34:23 +0300

    add 404 page

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,8 @@ const movieController = require('./controllers/movieController')
 router.use(homeConroller);
 router.use(movieController);
 
+router.get('*', (req, res) => {
+    res.redirect('/404');
+})
+
 module.exports = router;
