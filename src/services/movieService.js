4af3c4cf90bfea5c24ca73dbe2e41ef73262cd42
qnchoyy <qni191488@abv.bg>
2024-10-09 01:40:38 +0300

    add search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -16,6 +16,24 @@ exports.getAll = () => {
     return [...movies]
 }
 
+exports.search = (title, genre, year) => {
+    let result = movies.slice();
+
+    if (title) {
+        result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+    }
+
+    if (genre) {
+        result = result.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
+    }
+
+    if (year) {
+        result = result.filter(movie => movies.year === year);
+    }
+
+    return result;
+}
+
 exports.getOne = (movieId) => {
     const movie = movies.find(movie => movie._id == movieId);
 
