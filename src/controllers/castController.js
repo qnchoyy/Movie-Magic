24d8eb8c73bd052f1c6effaf39c4f0cc367cb03d
qnchoyy <qni191488@abv.bg>
2024-10-13 13:26:08 +0300

    add post action for cast create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 
 router.get('/create', (req, res) => {
     res.render('cast/create');
-})
+});
+
+router.post('/create', (req, res) => {
+    const body = req.body;
+
+    console.log(body);
+
+    res.redirect('/');
+});
 
 module.exports = router;
