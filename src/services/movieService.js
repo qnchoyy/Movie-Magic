83c9788776d544e4316038891ff164656609de34
qnchoyy <qni191488@abv.bg>
2024-10-13 11:42:45 +0300

    use mongoose for movie create

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,3 +1,5 @@
+const Movie = require('../models/Movie');
+
 const movies = [
     {
         _id: 1,
@@ -41,7 +43,5 @@ exports.getOne = (movieId) => {
 }
 
 exports.create = (movieData) => {
-
-    movieData._id = movies[movies.length - 1]._id + 1;
-    movies.push(movieData);
+    return Movie.create(movieData);
 }
