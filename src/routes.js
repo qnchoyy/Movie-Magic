088dd56590cbd5f37db76966fdc98618dc07953b
qnchoyy <qni191488@abv.bg>
2024-10-13 13:21:54 +0300

    add create cast page

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const homeConroller = require('./controllers/homeController');
 const movieController = require('./controllers/movieController')
+const castController = require('./controllers/castController');
 
 router.use(homeConroller);
 router.use(movieController);
+router.use('/cast', castController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
