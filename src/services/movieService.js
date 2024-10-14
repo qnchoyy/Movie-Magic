e8448ae8ccd8f03db0909c840b475ffe050be3f4
qnchoyy <qni191488@abv.bg>
2024-10-14 17:26:14 +0300

    fix search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -8,22 +8,22 @@ exports.getAll = () => {
 }
 
 //TODO : filter result in mongoDB
-exports.search = async (title, genre, year) => {
-    let result = await Movie.find().lean();
+exports.search = (title, genre, year) => {
+    let query = {}
 
     if (title) {
-        result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+        query.title = new RegExp(title, 'i');
     }
 
     if (genre) {
-        result = result.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
+        query.genre = genre.toLowerCase();
     }
 
     if (year) {
-        result = result.filter(movie => movies.year === year);
+        query.year = year;
     }
 
-    return result;
+    return Movie.find(query);
 }
 
 exports.getOne = (movieId) => Movie.findById(movieId).populate('casts');
