bd33a0c10eb167362a262015965c96138e41a260
qnchoyy <qni191488@abv.bg>
2024-10-16 20:37:11 +0300

    add register page

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const homeConroller = require('./controllers/homeController');
 const movieController = require('./controllers/movieController')
 const castController = require('./controllers/castController');
+const authController = require('./controllers/authController');
 
 router.use(homeConroller);
 router.use(movieController);
 router.use('/cast', castController);
+router.use('/auth', authController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
