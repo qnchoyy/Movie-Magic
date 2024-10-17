14b9e1dbdcd6dd0e7b2602e5964185c07446c0de
qnchoyy <qni191488@abv.bg>
2024-10-17 13:17:03 +0300

    add cookie-parser to express

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cookierParser = require('cookie-parser');
 
 function configExpress(app) {
     app.use(express.static(path.resolve('src/public')));
     app.use(express.urlencoded({ extended: false }));
+    app.use(cookierParser());
 
     return app;
 }
