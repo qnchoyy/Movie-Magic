5b872cfb6179777c603671d265b41659e2096223
qnchoyy <qni191488@abv.bg>
2024-10-20 12:16:38 +0300

    add dynamic links in the navbar

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -26,7 +26,7 @@ router.post('/create', isAuth, async (req, res) => {
 router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
-    const isOwner = movie.owner == req.user._id;
+    const isOwner = movie.owner == req.user?._id;
 
     //TODO: This is not perfect , use handlebars helpers
     movie.ratingStars = '&#x2605'.repeat(movie.rating);
