87892bb80d08b8dd447ae2c265a4b88a997ee65a
qnchoyy <qni191488@abv.bg>
2024-10-23 12:25:09 +0300

    throw error when email already exists

--- src/models/User.js
+++ src/models/User.js
@@ -7,9 +7,13 @@ const userSchema = new mongoose.Schema({
         required: true,
         lowercase: true,
         unique: true,
+        match: [/@[a-zA-Z]+\.[a-zA-Z0-9]+$/, 'Inavlid Email Adress'],
+        minLength: [10, 'Email should be at least 10 characters'],
     },
     password: {
         type: String,
+        match: [/^[a-zA-Z0-9]+$/, 'Password should be alphanumeric'],
+        minLength: [6, 'Password should be at least 6 characters long'],
         required: true,
     },
 });
--- src/services/authService.js
+++ src/services/authService.js
@@ -4,8 +4,14 @@ const jwt = require('../lib/jwt');
 const { SECRET } = require('../config/config');
 
 // TODO: Check if user exists
-exports.register = (userData) => User.create(userData);
+exports.register = (userData) => {
+    const user = User.findOne({ email: userData.email });
+    if (user) {
+        throw new Error('Email already exists');
+    }
 
+    return User.create(userData);
+}
 exports.login = async (email, password) => {
     // Get user from db
     const user = await User.findOne({ email });
